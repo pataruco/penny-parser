c7e0b502d8e7acca4cec4a1211f10b9694aecc3b
Pedro Martin <pataruco@gmail.com>
2017-10-03 22:59:34 +0100

    Set a test for two pounds

--- test/sterling-calculator-test.js
+++ test/sterling-calculator-test.js
@@ -16,10 +16,31 @@ describe('SterlingCalculator', ( ) => {
     describe('Output', ( ) => {
 
         describe('When you give a number', ( ) => {
+
             it('should return an object' , ( ) => {
                 const pennyParse = new PennyParser('432');
                 const calculator = new SterlingCalculator( pennyParse.number );
-                assert.isObject(calculator.calculate);;
+                assert.isObject(calculator.calculate);
+            })
+        }) ;
+
+        // 123 => { twoPounds: null, onePound: 1, twentyP: 1, tenP: null, fiveP: null, twoP: 1, oneP: 1}
+        describe('When parse 123', ( ) => {
+
+            it('should return an object with key twoPounds and value of null' , ( ) => {
+                const pennyParse = new PennyParser('123');
+                const calculator = new SterlingCalculator( pennyParse.number );
+                assert.equal(calculator.calculate['twoPounds'], null );;
+            })
+        }) ;
+
+        // 123 => { twoPounds: 6, onePound: null, twentyP: 1, tenP: 1, fiveP: null, twoP: 2, oneP: null}
+        describe('When parse 1234', ( ) => {
+
+            it('should return an object with key twoPounds and value of 2' , ( ) => {
+                const pennyParse = new PennyParser('1234');
+                const calculator = new SterlingCalculator( pennyParse.number );
+                assert.equal(calculator.calculate['twoPounds'], 6 );;
             })
         }) ;
 
