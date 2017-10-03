15b7fbdbb05f547646ccde9c2a3965d3ab2723b3
Pedro Martin <pataruco@gmail.com>
2017-10-03 22:24:39 +0100

    Create a class sterling calculator

---
+++ assets/js/sterling-calculator.js
@@ -0,0 +1,11 @@
+class SterlingCalculator {
+    constructor( number ) {
+        this.penniesAmmount = number;
+    }
+
+    get calculate( ){
+        return { }
+    }
+}
+
+module.exports = SterlingCalculator;
