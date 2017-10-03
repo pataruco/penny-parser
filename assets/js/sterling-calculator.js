678509e7b7081ac04c4a49ccba1761ef999f0f41
Pedro Martin <pataruco@gmail.com>
2017-10-03 23:50:11 +0100

    Set SterlingCalculator test and create class

--- assets/js/sterling-calculator.js
+++ assets/js/sterling-calculator.js
@@ -4,6 +4,7 @@ class SterlingCalculator {
         this.calculation = {
             twoPounds: null,
             onePound: null,
+            fiftyP: null,
             twentyP: null,
             tenP: null,
             fiveP: null,
@@ -17,14 +18,93 @@ class SterlingCalculator {
     }
 
     calculateTwoPounds( ) {
-        if ( ( this.penniesAmmount / 200 ) > 1  ) {
+        const remainder = this.penniesAmmount % 200;
+
+        if ( ( this.penniesAmmount / 200 ) >= 1  ) {
             this.calculation.twoPounds = parseInt( this.penniesAmmount / 200 );
-            return this.calculation;
         } else {
             this.calculation.twoPounds = null;
-            return this.calculation;
-
         }
+
+        return this.calculateOnePound( remainder )
+    }
+
+    calculateOnePound( amount ) {
+        const remainder = amount % 100;
+
+        if ( ( amount / 100 ) >= 1  ) {
+            this.calculation.onePound = parseInt( amount / 100 );
+        } else {
+            this.calculation.onePound = null;
+        }
+
+        return this.calculateFiftyP( remainder );
+    }
+
+    calculateFiftyP( amount ) {
+        const remainder = amount % 50;
+
+        if ( ( amount / 50 ) >= 1  ) {
+            this.calculation.fiftyP = parseInt( amount / 50 );
+        } else {
+            this.calculation.fiftyP = null;
+        }
+
+        return this.calculateTwentyP( remainder );
+    }
+
+    calculateTwentyP( amount ) {
+        const remainder = amount % 20;
+
+        if ( ( amount / 20 ) >= 1  ) {
+            this.calculation.twentyP = parseInt( amount / 20 );
+        } else {
+            this.calculation.twentyP = null;
+        }
+
+        return this.calculateTenP( remainder );
+    }
+
+    calculateTenP( amount ) {
+        const remainder = amount % 10;
+
+        if ( ( amount / 10 ) >= 1  ) {
+            this.calculation.tenP = parseInt( amount / 10 );
+        } else {
+            this.calculation.tenP = null;
+        }
+        return this.calculateFiveP( remainder );
+    }
+
+    calculateFiveP( amount ) {
+        const remainder = amount % 5;
+
+        if ( ( amount / 5 ) >= 1  ) {
+            this.calculation.fiveP = parseInt( amount / 5 );
+        } else {
+            this.calculation.fiveP = null;
+        }
+        return this.calculateTwoP( remainder );
+    }
+
+    calculateTwoP( amount ) {
+        const remainder = amount % 2;
+
+        if ( ( amount / 2 ) >= 1  ) {
+            this.calculation.twoP = parseInt( amount / 2 );
+        } else {
+            this.calculation.twoP = null;
+        }
+        return this.calculateOneP( remainder );
+    }
+
+    calculateOneP( amount ) {
+        if ( amount  >= 1  ) {
+            this.calculation.oneP = amount
+        } else {
+            this.calculation.oneP = null;
+        }
+        return this.calculation;
     }
 }
 
--- test/sterling-calculator-test.js
+++ test/sterling-calculator-test.js
@@ -24,23 +24,81 @@ describe('SterlingCalculator', ( ) => {
             })
         }) ;
 
-        // 123 => { twoPounds: null, onePound: 1, twentyP: 1, tenP: null, fiveP: null, twoP: 1, oneP: 1}
+        // 123 => { twoPounds: null, onePound: 1, fiftyP: null, twentyP: 1, tenP: null, fiveP: null, twoP: 1, oneP: 1}
         describe('When parse 123', ( ) => {
 
+            const pennyParse = new PennyParser('123');
+            const calculator = new SterlingCalculator( pennyParse.number );
+
             it('should return an object with key twoPounds and value of null' , ( ) => {
-                const pennyParse = new PennyParser('123');
-                const calculator = new SterlingCalculator( pennyParse.number );
-                assert.equal(calculator.calculate['twoPounds'], null );;
+                assert.equal(calculator.calculate['twoPounds'], null );
+            })
+
+            it('should return an object with key onePound and value of 1' , ( ) => {
+                assert.equal(calculator.calculate['onePound'], 1 );
+            })
+
+            it('should return an object with key fiftyP and value of null' , ( ) => {
+                assert.equal(calculator.calculate['fiftyP'], null );
+            })
+
+            it('should return an object with key twentyP and value of 1' , ( ) => {
+                assert.equal(calculator.calculate['twentyP'], 1 );
+            })
+
+            it('should return an object with key tenP and value of null ' , ( ) => {
+                assert.equal(calculator.calculate['tenP'], null );
+            })
+
+            it('should return an object with key fiveP and value of null ' , ( ) => {
+                assert.equal(calculator.calculate['fiveP'], null );
+            })
+
+            it('should return an object with key twoP and value of 1 ' , ( ) => {
+                assert.equal(calculator.calculate['twoP'], 1 );
+            })
+
+            it('should return an object with key oneP and value of 1 ' , ( ) => {
+                assert.equal(calculator.calculate['oneP'], 1 );
             })
         }) ;
 
-        // 123 => { twoPounds: 6, onePound: null, twentyP: 1, tenP: 1, fiveP: null, twoP: 2, oneP: null}
+        // 123 => { twoPounds: 6, onePound: null, fiftyP: null, twentyP: 1, tenP: 1, fiveP: null, twoP: 2, oneP: null}
         describe('When parse 1234', ( ) => {
 
+            const pennyParse = new PennyParser('1234');
+            const calculator = new SterlingCalculator( pennyParse.number );
+
             it('should return an object with key twoPounds and value of 2' , ( ) => {
-                const pennyParse = new PennyParser('1234');
-                const calculator = new SterlingCalculator( pennyParse.number );
-                assert.equal(calculator.calculate['twoPounds'], 6 );;
+                assert.equal(calculator.calculate['twoPounds'], 6 );
+            })
+
+            it('should return an object with key onePound and value of null' , ( ) => {
+                assert.equal(calculator.calculate['onePound'], null );
+            })
+
+            it('should return an object with key fiftyP and value of null' , ( ) => {
+                assert.equal(calculator.calculate['fiftyP'], null );
+            })
+
+            it('should return an object with key twentyP and value of 1' , ( ) => {
+                assert.equal(calculator.calculate['twentyP'], 1 );
+            })
+
+            it('should return an object with key tenP and value of 1 ' , ( ) => {
+                assert.equal(calculator.calculate['tenP'], 1 );
+            })
+
+            it('should return an object with key fiveP and value of null ' , ( ) => {
+                assert.equal(calculator.calculate['fiveP'], null );
+            });
+
+            it('should return an object with key twoP and value of 1 ' , ( ) => {
+                assert.equal(calculator.calculate['twoP'], 2 );
+            })
+
+            it('should return an object with key oneP and value of null ' , ( ) => {
+                assert.equal(calculator.calculate['oneP'], null );
             })
         }) ;
 
