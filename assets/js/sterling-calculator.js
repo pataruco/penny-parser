9e8b91d9ad1e6ab0ffa82847010ce7a3e3436b73
Pedro Martin <pataruco@gmail.com>
2017-10-03 22:59:59 +0100

    Create a method to calculate two pounds

--- assets/js/sterling-calculator.js
+++ assets/js/sterling-calculator.js
@@ -1,10 +1,30 @@
 class SterlingCalculator {
     constructor( number ) {
         this.penniesAmmount = number;
+        this.calculation = {
+            twoPounds: null,
+            onePound: null,
+            twentyP: null,
+            tenP: null,
+            fiveP: null,
+            twoP: null,
+            oneP: null
+        }
     }
 
     get calculate( ){
-        return { }
+        return this.calculateTwoPounds( )
+    }
+
+    calculateTwoPounds( ) {
+        if ( ( this.penniesAmmount / 200 ) > 1  ) {
+            this.calculation.twoPounds = parseInt( this.penniesAmmount / 200 );
+            return this.calculation;
+        } else {
+            this.calculation.twoPounds = null;
+            return this.calculation;
+
+        }
     }
 }
 
