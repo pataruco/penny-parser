5c96d87960ca5759a46a4128bf7e2d166aabe4e8
Pedro Martin <pataruco@gmail.com>
2017-10-03 21:46:32 +0100

    Create a method to return missing values error and refactor

--- assets/js/penny-parser.js
+++ assets/js/penny-parser.js
@@ -1,61 +1,69 @@
 class PennyParser {
-     constructor( string ) {
-          this.string = string;
-          this.isPInTheLastPosition =  string.lastIndexOf('p') === ( string.length -1 );
-          this.isSterlingInTheFirstPosition = string.indexOf('£') === 0;
-          this.isDotPresent = string.includes('.');
-          const areValidCharactersPattern = new RegExp( '^[0-9\.\p\£\]*$' )
-          this.areValidCharacters = areValidCharactersPattern.test( string );
-     }
+    constructor(string) {
+        this.string = string;
+        this.isPInTheLastPosition = string.lastIndexOf('p') === (string.length - 1);
+        this.isSterlingInTheFirstPosition = string.indexOf('£') === 0;
+        this.isDotPresent = string.includes('.');
+        const areValidCharactersPattern = new RegExp('^[0-9\.\p\£\]*$')
+        this.areValidCharacters = areValidCharactersPattern.test(string);
+        const areNumbersNotPresentPattern = new RegExp('^([^0-9]*)$');
+        this.areNumbersNotPresent = areNumbersNotPresentPattern.test(string);
+    }
 
-     get number( ) {
-          if ( this.isSterlingInTheFirstPosition &&  this.isPInTheLastPosition && this.areValidCharacters ) {
-               return this.sterlingConvertToNumber( );
-          }
+    get number() {
+        if (this.areNumbersNotPresent && this.isSterlingInTheFirstPosition && this.isPInTheLastPosition) {
+            return this.missingValuesError()
+        }
 
-          if ( this.isSterlingInTheFirstPosition && this.areValidCharacters ) {
-               return this.sterlingConvertToNumber( );
-          }
+        if (this.isSterlingInTheFirstPosition && this.isPInTheLastPosition && this.areValidCharacters) {
+            return this.sterlingConvertToNumber();
+        }
 
-          if ( this.isPInTheLastPosition && this.isDotPresent && this.areValidCharacters ) {
-               return this.pDotConvertToNumber( );
-          }
+        if (this.isSterlingInTheFirstPosition && this.areValidCharacters) {
+            return this.sterlingConvertToNumber();
+        }
 
-          if ( this.isPInTheLastPosition && this.areValidCharacters ) {
-               return this.pConvertToNumber( );
-          }
+        if (this.isPInTheLastPosition && this.isDotPresent && this.areValidCharacters) {
+            return this.pDotConvertToNumber();
+        }
 
-          if ( !this.areValidCharacters ) {
-              return this.invalidCharacterError( )
-          }
+        if (this.isPInTheLastPosition && this.areValidCharacters) {
+            return this.pConvertToNumber();
+        }
 
+        if (!this.areValidCharacters) {
+            return this.invalidCharacterError()
+        }
 
-          return this.clearString (  );
-     }
+        return this.clearString();
+    }
 
-     sterlingConvertToNumber( ) {
-          const stringNumber = this.string.replace(/p/g, '').replace(/£/,'');
-          return Math.floor( parseFloat( stringNumber ) * 100 );
-     }
+    sterlingConvertToNumber() {
+        const stringNumber = this.string.replace(/p/g, '').replace(/£/, '');
+        return Math.floor(parseFloat(stringNumber) * 100);
+    }
 
-     pConvertToNumber( ) {
-          const stringNumber = this.string.replace(/p/g, '');
-          return parseFloat( stringNumber );
-     }
+    pConvertToNumber() {
+        const stringNumber = this.string.replace(/p/g, '');
+        return parseFloat(stringNumber);
+    }
 
-     pDotConvertToNumber( ) {
-          const stringNumber = this.string.replace(/p/g, '');
-          return parseFloat( stringNumber ) * 100;
-     }
+    pDotConvertToNumber() {
+        const stringNumber = this.string.replace(/p/g, '');
+        return parseFloat(stringNumber) * 100;
+    }
 
-     invalidCharacterError( ) {
-         return new Error('invalid character');
-     }
+    invalidCharacterError() {
+        return new Error('invalid character');
+    }
 
-     clearString(  ) {
-          return parseFloat ( this.string.replace(/p/g, '').replace(/£/,'') );
-     }
+    missingValuesError() {
+        return new Error('missing values');
+    }
+
+    clearString() {
+        return parseFloat(this.string.replace(/p/g, '').replace(/£/, ''));
+    }
 }
 
-
-module.exports  = PennyParser;
+module.exports = PennyParser;
