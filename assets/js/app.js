a5fc85f8298446589b02494c3967c604bc7a3ba6
Pedro Martin <pataruco@gmail.com>
2017-10-04 19:28:42 +0100

    //WIP Backup

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+const PennyParser = require('./penny-parser.js');
+const SterlingCalculator = require('./sterling-calculator.js');
+const Vue = require('vue');
+
+
+document.addEventListener("DOMContentLoaded", () =>  {
+    const app = new Vue({
+        el: '#js-app',
+        data: {
+            calculateString: '',
+            sterling: { }
+        },
+        methods: {
+            onSubmit(  ) {
+                let penniesAmount;
+
+                try {
+                    penniesAmount = new PennyParser( this.calculateString ).number;
+
+                } catch (e) {
+                    console.error(error.message);
+                } finally {
+                    this.sterling = new SterlingCalculator( penniesAmount ).calculate;
+                    console.log( this.sterling );
+                }
+            }
+        }
+    });// end of Vue
+}); // end of document
